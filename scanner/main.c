732d453e64889083c9565d86b0529d5ddf26008e
Jiwon <raipen@knu.ac.kr>
2022-04-15 13:09:22 +0900

    asdfsdf

--- scanner/main.c
+++ scanner/main.c
@@ -2,9 +2,6 @@
 #include <stdio.h>
 #include <ctype.h>
 #define MAX_BUFF_SIZE 1000
-#define TRUE 1
-#define FALSE 0
-#define BOOLEAN int
 #define DEBUG
 #ifdef DEBUG
 #define debug(a) a
@@ -17,11 +14,49 @@ typedef enum{
     START, INEQ, INCOMMENT, INNUM, INID, DONE, INLT, INGT, INNE, INOVER, INCOMMENT_
 }StateType;
 typedef enum {
-    SAVE, IGNORE, BACK
+    SAVE, IGNORE, BACK, STAY
 }LookAhead;
 typedef enum {
-    ID, NUM
+    /*0~2*/
+    ERROR, ID, NUM,
+    /* 예약어(3~8) */
+   ELSE, IF, INT, RETURN, VOID, WHILE,
+    /* special symbols(9~27)*/
+    PLUS, MINUS, TIMES,DIVIDE,LESS,LESS_EQUAL,GREATER,GREATER_EQUAL, EQUAL, NOT_EQUAL,ASSIGN,SEMICOLON,COMMA, LPAREN, RPAREN, LBRACE, RBRACE, LSQUARE, RSQUARE
 }TokenType;
+#define RESERVED_TOKEN_COUNT 6
+struct {
+    char* string;
+    TokenType type;
+}ReservedToken[RESERVED_TOKEN_COUNT] = { {"else",ELSE},{"if",IF},{"int",INT},{"return",RETURN},{"void",VOID},{"while",WHILE} };
+
+TokenType findKeywords(char* tokenString) {
+    for (int i = 0; i < RESERVED_TOKEN_COUNT; i++)
+        if (!strcmp(tokenString, ReservedToken[i].string))
+            return ReservedToken[i].type;
+    return ID;
+}
+
+#define FIRST_SPECIAL_SYMBOL PLUS
+char specialSymbols[][3] = { "+","-","*","/","<","<=",">",">=","==","!=","=",";",",","(",")","{","}","[","]" };
+void printToken() {
+    switch (curToken) {
+    case ERROR:
+        fprintf(OUTPUT, "\t%4d: ERROR: %s\n", curLine, curTokString);
+        break;
+    case ID:
+        fprintf(OUTPUT, "\t%4d: ID, val= %s\n", curLine, curTokString);
+        break;
+    case NUM:
+        fprintf(OUTPUT, "\t%4d: NUM, val= %s\n", curLine, curTokString);
+        break;
+    default:
+        if (curToken < 9)
+            fprintf(OUTPUT, "\t%4d: reserved word: % s\n", curLine, curTokString);
+        else
+            fprintf(OUTPUT, "\t%4d: %s\n", curLine, specialSymbols[curToken- FIRST_SPECIAL_SYMBOL]);
+    }
+}
 
 FILE* input,* output;//입력 파일과 출력 파일
 StateType state = START; //현재 상태
@@ -39,7 +74,7 @@ void initInput(int argc, char* argv[]) {
     input = fopen(argv[1], "r");
     output = fopen(argv[2], "w");
     if (input == NULL) {
-        fprintf(OUTPUT,"No input file");
+        fprintf(OUTPUT,"No input file <%s>",argv[1]);
         exit(1);
     }
 }
@@ -60,6 +95,61 @@ LookAhead checkAhead(char c) {
             switch (c) {
             case ' ': case '\t': case '\n':
                 return IGNORE;
+            case '/':
+                state = INOVER;
+                break;
+            case '=':
+                state = INEQ;
+                break;
+            case '<':
+                state = INLT;
+                break;
+            case '>':
+                state = INGT;
+                break;
+            case '!':
+                state = INNE;
+                break;
+            default:
+                switch (c) {
+                case '+':
+                    curToken = PLUS;
+                    break;
+                case '-':
+                    curToken = MINUS;
+                    break;
+                case '*':
+                    curToken = TIMES;
+                    break;
+                case ';':
+                    curToken = SEMICOLON;
+                    break;
+                case ',':
+                    curToken = COMMA;
+                    break;
+                case '(':
+                    curToken = LPAREN;
+                    break;
+                case ')':
+                    curToken = RPAREN;
+                    break;
+                case '{':
+                    curToken = LBRACE;
+                    break;
+                case '}':
+                    curToken = RBRACE;
+                    break;
+                case '[':
+                    curToken = LSQUARE;
+                    break;
+                case ']':
+                    curToken = RSQUARE;
+                    break;
+                default:
+                    curToken = ERROR;
+                    break;
+                }
+                return STAY;
             }
         }
         break; //end of START case
@@ -83,17 +173,6 @@ LookAhead checkAhead(char c) {
     return SAVE;
 }
 
-void printToken() {
-    switch (curToken) {
-    case ID:
-        fprintf(OUTPUT, "\t%4d: ID, val= %s\n", curLine, curTokString);
-        break;
-    case NUM:
-        fprintf(OUTPUT, "\t%4d: NUM, val= %s\n", curLine, curTokString);
-        break;
-    }
-}
-
 int main(int argc, char* argv[]) {
     initInput(argc,argv);
     char lineHold[MAX_BUFF_SIZE];
@@ -102,20 +181,24 @@ int main(int argc, char* argv[]) {
         fprintf(OUTPUT, "%4d: %s", ++curLine, lineHold);
         for (int i = 0; lineHold[i]; i++) {
             switch (checkAhead(lineHold[i])) {
-            case SAVE:
+            case SAVE: //현재 문자를 토큰에 추가
                 curTokString[curTokStringIndex++] = lineHold[i];
                 break;
             case IGNORE:
                 // do nothing
                 break;
-            case BACK:
+            case BACK:  //현재 문자 이전에서 상태 완료. 완성된 토큰에 대한 작업이 들어가면 됨.
                 i--;
+            case STAY: //현재 문자까지 해서 상태 완료.
                 curTokString[curTokStringIndex] = '\0';
+                curToken = curToken == ID ? findKeywords(curTokString): curToken;
                 printToken();
-                initState();
+
+                initState(); //처음 상태로 초기화
                 break;
             }
         }
     }
-    fprintf(OUTPUT,"\t%4d: EOF",++curLine);
+    fprintf(OUTPUT,"\t%4d: EOF\n",++curLine);
+    return 0;
 }
